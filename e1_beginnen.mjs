c97959185a8e47e77113aa40019828799b29db74
Younes Aki <younesakichouh@hotmail.com>
2023-09-28 16:10:23 +0200

    Update e1_beginnen.mjs
    Eerste easy oef gedaan.

--- e1_beginnen.mjs
+++ e1_beginnen.mjs
@@ -1,5 +1,9 @@
-// Voeg hier je eigen code in
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
 
-
+let name = 'Younes';// Voeg hier je eigen code in
+let age = 19 ;
+let favoriteGame = 'Fifa';
 
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
