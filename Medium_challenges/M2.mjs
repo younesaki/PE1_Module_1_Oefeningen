f4555d5127085986b1313f810b4875d5fd775c93
Younes Aki <younesakichouh@hotmail.com>
2023-10-02 10:16:37 +0200

    Finished M2

--- Medium_challenges/M2.mjs
+++ Medium_challenges/M2.mjs
@@ -2,3 +2,11 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
+let geboorteJaar = 2004;
+let toekomstJaar = 2044;
+
+toekomstJaar = toekomstJaar - geboorteJaar ;
+
+console.log('In 2044 zal ik ' + toekomstJaar-- + ' of ' + toekomstJaar + ' jaar oud zijn.');
+
+process.exit();
